8c06af2ff0afc7e478697fda0cfe22d1c96902fd
pankonrad <kgrabowski@infovidematrix.pl>
2016-04-09 23:36:11 +0200

    mainpagecontroller updated 1.1

--- src/main/java/pl/sidgroup/sweeper/tools/MainPageController.java
+++ src/main/java/pl/sidgroup/sweeper/tools/MainPageController.java
@@ -1,26 +1,13 @@
 package pl.sidgroup.sweeper.tools;
 
 import javax.inject.Named;
-import javax.enterprise.inject.Model;
 
-@Model
 @Named(value="mainPageController")
 public class MainPageController {
-	
-//	@EJB
-//	private ConfigurationProvider configurationProvider;
-//	@EJB
-//	private RemoteConfigurationProvider remoteConfigurationProvider;
-	
+
 	private String message;
 	
 	public String reloadConfiguration() {
-//		boolean sweeperConfig = configurationProvider.reloadConfiguration();
-//		boolean remoteConfig = remoteConfigurationProvider.reloadConfiguration();
-		
-//		message = sweeperConfig && remoteConfig ? 
-//				"konfiguracja wczytana" : "Bład podczas wczytywania konfiguracji " 
-//				+ (sweeperConfig ? "sweeperConfig !" : "remoteConfig !");
 
 		message = "konfiguracja wczytana!";
 		return message;
